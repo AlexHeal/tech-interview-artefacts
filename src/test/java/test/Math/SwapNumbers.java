5a20172cfb6536fd0a572e0ec0b192ebf74d4d02
Alex Heal <24870446+AlexHeal@users.noreply.github.com>
2022-06-29 09:48:18 +0500

    Create SwapNumbers.java

---
+++ src/test/java/test/Math/SwapNumbers.java
@@ -0,0 +1,17 @@
+package test.Math;
+
+import org.junit.jupiter.api.Test;
+
+public class SwapNumbers {
+    void swap(int a, int b) {
+        a = a - b;
+        b = b + a;
+        a = b - a;
+        System.out.println(a + " " + b);
+    }
+
+    @Test
+    void name() {
+        swap(1, 2);
+    }
+}
