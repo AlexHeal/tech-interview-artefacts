0924c538c4ff1ed55f2c14c551a1701f5d6c8fae
Alex Heal <24870446+AlexHeal@users.noreply.github.com>
2022-06-29 09:49:58 +0500

    Update SwapNumbers.java

--- src/test/java/test/Math/SwapNumbers.java
+++ src/test/java/test/Math/SwapNumbers.java
@@ -10,8 +10,16 @@ public class SwapNumbers {
         System.out.println(a + " " + b);
     }
 
+    void binarySwap(int a, int b) {
+        a = a ^ b;
+        b = b ^ a;
+        a = b ^ a;
+        System.out.println(a + " " + b);
+    }
+
     @Test
     void name() {
         swap(1, 2);
+        binarySwap(1, 2);
     }
 }
