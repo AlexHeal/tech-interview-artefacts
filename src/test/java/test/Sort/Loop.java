4b030a9338140df7256e8ef0dbb388679127a068
Alex Heal <24870446+AlexHeal@users.noreply.github.com>
2022-06-30 10:50:23 +0500

    Create Loop.java

---
+++ src/test/java/test/Sort/Loop.java
@@ -0,0 +1,29 @@
+package test.Sort;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class Loop {
+    int[] loopSort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[i]) {
+                    arr[i] = arr[i] ^ arr[j];
+                    arr[j] = arr[i] ^ arr[j];
+                    arr[i] = arr[j] ^ arr[i];
+                }
+            }
+        }
+
+        return arr;
+    }
+
+    @Test
+    void name() {
+        int[] arr = {4, 5, 3, 2, 1, 4, 4, 5};
+        int[] expected = {1, 2, 3, 4, 4, 4, 5, 5};
+
+        assertThat(expected).isEqualTo(loopSort(arr));
+    }
+}
